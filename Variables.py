e116fefc5373f17f201f92fb8a0fbddcaea92a80
ILoveCoding-123 <Kridagarwal@gmail.com>
2026-06-11 21:32:04 -0400

    Add files via upload

---
+++ Variables.py
@@ -0,0 +1,11 @@
+x = "soccer"
+y = "cricket"
+print(x)
+print(y)
+
+Messi_and_Kohli = "Messi and Kohli"
+print(Messi_and_Kohli)
+
+Favorite_Sport = input("What is your favorite sport? ")
+
+print("\nWhat are famous players of your sport?")
