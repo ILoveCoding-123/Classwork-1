63674e9c0ba12bc719cfca2d037d0065f83a575b
ILoveCoding-123 <Kridagarwal@gmail.com>
2026-06-21 20:03:24 -0400

    Add files via upload

---
+++ SummerTimes.py
@@ -0,0 +1,9 @@
+Fahrenheit = int(input("Enter the temperature:"))
+if Fahrenheit >= 70:
+    print("It's Summer Time!")
+    print("We can eat ice cream!")
+    print("We can go to the beach!")
+else:
+    print("It's not Summer Time!")
+    print("We can't eat ice cream!")
+    print("We can't go to the beach!")
