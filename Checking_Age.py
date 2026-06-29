da3e3f83b1efea6b470a3814246591b31ee46735
ILoveCoding-123 <Kridagarwal@gmail.com>
2026-06-28 20:02:21 -0400

    Add files via upload

---
+++ Checking_Age.py
@@ -0,0 +1,16 @@
+print("Select your Age Group: ")
+print("1. Under 18")
+print("2. 18 and above")
+
+
+choice = int( input("Enter your choice: ") )
+
+ 
+if( choice == 1 ): 
+    print("You are not allowed to watch this movie.")
+
+else:
+    if( choice == 2 ):
+        print("You are allowed to watch this movie")
+
+
