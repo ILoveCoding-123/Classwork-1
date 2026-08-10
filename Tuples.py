e50a43e3adbaf42bb6aa187a3d11ddbd88751b90
ILoveCoding-123 <Kridagarwal@gmail.com>
2026-08-09 20:35:54 -0400

    Add files via upload

---
+++ Tuples.py
@@ -0,0 +1,31 @@
+tuplex = ("tuple", False, 3.2, 1)
+print(tuplex)
+
+
+tuplex1 = (4, 6, 2, 8, 3, 1)
+print(tuplex1)
+
+
+tuplex2 = tuplex1 + (9,)
+print(tuplex2)
+
+
+tuple1 = (50, 10, 60, 70, 50)
+print(tuple1.count(50))
+
+
+tuplex3 = (2, 4, 3, 5, 4, 6, 7, 8, 6, 1)
+
+
+_slice = tuplex3[3:5]
+
+
+print(_slice)
+
+
+_slice1 = tuplex3[:6]
+print(_slice1)
+
+
+
+
